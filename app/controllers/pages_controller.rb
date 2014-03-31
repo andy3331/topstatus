082ee3aae2c8ec640801dc7a0bcdef88f20916ae
Andy Connington <aconnington@gmail.com>
2014-03-30 22:19:38 -0400

    adding routes logos etc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def home
   end
 
+  def submit
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Topstatus::Application.routes.draw do
 root "pages#home"
+get "submit" => "pages#submit"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
